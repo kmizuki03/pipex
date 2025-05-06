4dd916b604b8a1c16046357b0ce1c902841a2e83
kmizuki03 <kt007_yokohama@icloud.com>
2025-05-06 11:25:03 +0900

    pipex

--- src.c
+++ src.c
@@ -6,7 +6,7 @@
 /*   By: kato <kato@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 18:02:42 by kato              #+#    #+#             */
-/*   Updated: 2025/05/05 18:45:32 by kato             ###   ########.fr       */
+/*   Updated: 2025/05/05 18:58:56 by kato             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,21 @@ void	execute_cmd1(char *infile, int *pipefd, char **args, char **envp)
 	fd = open(infile, O_RDONLY);
 	if (fd < 0)
 		perror_exit(infile);
-	dup2(fd, STDIN_FILENO);
-	dup2(pipefd[1], STDOUT_FILENO);
+	if (dup2(fd, STDIN_FILENO) < 0)
+		perror_exit("dup2");
+	if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+		perror_exit("dup2");
 	close(fd);
 	close(pipefd[0]);
 	close(pipefd[1]);
-	execve("/bin/sh", args, envp);
+	if (!args || !args[0])
+	{
+		ft_putstr_fd("pipex: invalid command\n", 2);
+		exit(127);
+	}
+	execve(args[0], args, envp);
 	ft_putstr_fd("pipex: ", 2);
-	perror(args[2]);
+	perror(args[0]);
 	exit(127);
 }
 
@@ -37,13 +44,20 @@ void	execute_cmd2(char *outfile, int *pipefd, char **args, char **envp)
 	fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd < 0)
 		perror_exit(outfile);
-	dup2(pipefd[0], STDIN_FILENO);
-	dup2(fd, STDOUT_FILENO);
+	if (dup2(pipefd[0], STDIN_FILENO) < 0)
+		perror_exit("dup2");
+	if (dup2(fd, STDOUT_FILENO) < 0)
+		perror_exit("dup2");
 	close(fd);
 	close(pipefd[0]);
 	close(pipefd[1]);
-	execve("/bin/sh", args, envp);
+	if (!args || !args[0])
+	{
+		ft_putstr_fd("pipex: invalid command\n", 2);
+		exit(127);
+	}
+	execve(args[0], args, envp);
 	ft_putstr_fd("pipex: ", 2);
-	perror(args[2]);
+	perror(args[0]);
 	exit(127);
 }
